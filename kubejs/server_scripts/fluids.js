8e8eb35166c2da205eb0579f1e3f326dbb107bba
Paul-Andre Bisson <bissonpaulandre@gmail.com>
2025-01-25 14:17:28 -0500

    Removed unused data and improved replacement

--- kubejs/server_scripts/fluids.js
+++ kubejs/server_scripts/fluids.js
@@ -7,19 +7,19 @@ console.info('Hello, World! (Loaded startup scripts)')
 ServerEvents.recipes(event => {
 
     [
-        'nuclearcraft:bronze'
-    ].forEach((fluid) => {
+        ['nuclearcraft:bronze', 'tconstruct:molten_bronze']
+    ].forEach((replacementInfo) => {
+        const [fluid, replacement] = replacementInfo;
         event.forEachRecipe({mod: "nuclearcraft"}, recipe => {
             //console.info(`Recipe matched: ${recipe.json}` );
             if (recipe.json && recipe.json.get("inputFluids") != undefined) {
                 let changedFlag = false;
                 recipe.json.getAsJsonArray("inputFluids").forEach(inputFluid => {
-                    console.info(`looping: ${inputFluid}` );
-                    //console.info(`looping: ${inputFluid.getAsJsonObject("fluid")}` );
+                    //console.info(`looping: ${inputFluid}` );
                     if (inputFluid.get('fluid') && inputFluid.get('fluid').asString === fluid) {
-                        inputFluid.addProperty("fluid", 'tconstruct:molten_bronze') // will overwrite
+                        //console.info(`Recipe matched: ${replacement}` );
+                        inputFluid.addProperty("fluid", replacement) // will overwrite
                         changedFlag = true;
-                        //inputFluid['fluid'] = 'tconstruct:molten_bronze';
                     }
                 });
                 if (changedFlag) {
@@ -28,8 +28,6 @@ ServerEvents.recipes(event => {
             }
         });
     });
-    const oldFluid = 'minecraft:water'; // Fluid to replace
-    const newFluid = 'minecraft:lava'; // Fluid to use instead
     
     //event.replaceInput({ type: 'fluid', ingredient: { fluid: oldFluid } }, { fluid: oldFluid }, { fluid: newFluid });
     event.replaceInput(
@@ -38,24 +36,4 @@ ServerEvents.recipes(event => {
         '#minecraft:saplings'         // Arg 3: the item to replace it with
         // Note: tagged fluid ingredients do not work on Fabric, but tagged items do.
     );
-
-    //event.forEachRecipe("*", recipe => {
-    //    console.info(`Recipe matched: ${recipe.json}`);
-    //    if (recipe.json && recipe.json.input?.fluid === 'nuclearcraft:bronze') {
-    //        //console.info(`Recipe matched: ${recipe.id}`);
-    //    }
-    //});
-
-    //event.replaceInput(
-    //    "*", // Arg 1: the filter
-    //    Fluid.of('nuclearcraft:bronze', 144),            // Arg 2: the item to replace
-    //    Fluid.of('tconstruct:molten_bronze', 144)        // Arg 3: the item to replace it with
-    //    // Note: tagged fluid ingredients do not work on Fabric, but tagged items do.
-    //);
-    //event.replaceOutput(
-    //    "*", // Arg 1: the filter
-    //    Fluid.LAVA_ID,            // Arg 2: the item to replace
-    //    '#minecraft:saplings'         // Arg 3: the item to replace it with
-    //    // Note: tagged fluid ingredients do not work on Fabric, but tagged items do.
-    //);
 });
